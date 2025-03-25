6387b33a1e412254d150c64a8efe5888a36f6424
Hasan Raza <hasanraza102515209@gmail.com>
2025-03-25 15:18:19 +0530

    using cursor

--- backend/payUIntegeration.js
+++ backend/payUIntegeration.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { makePayment, verifyPayment, paymentWebhook } from "./controller/payment.js";
+import { makePayment, verifyPayment} from "./controller/payment.js";
 
 const router = express.Router();
 
 router.post("/pay", makePayment);       // Route for initiating payment
 router.post('/verify', verifyPayment);  // Route for verifying payment
-router.post('/webhook', paymentWebhook); // Route for PayU webhook
 
 export default router;
