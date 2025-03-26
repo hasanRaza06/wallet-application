d9454fa731d0935922841aa44a8fcc3507d3f9cd
Hasan Raza <hasanraza102515209@gmail.com>
2025-03-26 15:56:38 +0530

    upload original

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-import React, { StrictMode } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { CssBaseline } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <> <CssBaseline />
-   <StrictMode>
-    
       <App />
-      </StrictMode>
       </>
 )
